8a6e8a88f54d7f5b3e031ac9c31a0e8c012a4fe6
jwhit3 <36378184+jwhit3@users.noreply.github.com>
2019-08-19 16:42:04 -0400

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/AddServer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class AddServer : Form
+    {
+        string server = null;
+        string password = null;
+        public AddServer()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            server = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox2.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("C:\\Projects\\Passman\\database\\server.txt",append:true);
+                sw.WriteLine(server + "," + password);
+                sw.Close();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            
+        }
+
+       
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<string> _items = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Projects\Passman\database\server.txt");
+            foreach (string line in lines)
+                _items.Add(line);
+            //_items.Add("One");
+            //_items.Add("Two");
+            //_items.Add("Three");
+
+            listBox1.DataSource = _items;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Add button Clicked
+            MessageBox.Show("Adding server");
+            _items.Add("New Item" + DateTime.Now.Second);
+            this.Hide();
+            AddServer f2 = new AddServer();
+            f2.ShowDialog();
+            listBox1.DataSource = null;
+            listBox1.DataSource = _items;
+            this.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+
+            try
+            {
+                // Remove Items in the list
+                _items.RemoveAt(selectedIndex);
+            }
+            catch
+            {
+
+            }
+            listBox1.DataSource = null;
+            listBox1.DataSource = _items;
+        }
+    }
+}
